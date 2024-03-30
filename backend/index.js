761bbe456b073b143264e7daddac19986cf7d3cc
M.Abubakar Siddique <dev.abubakarsiddique@gmail.com>
2024-03-30 19:20:13 +0500

    Deployment try x3

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,11 @@ const candidateProfileRouter = require('./router/candidateProfileRouter');
 const employerProfileRouter = require('./router/employerProfileRouter'); 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+}));
 app.use(require("./router/uploadResumeRoute"))
 app.use(require("./router/authentication"))
 app.use(require("./router/home"))
