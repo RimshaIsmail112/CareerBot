849fab17137e8b83f8f5fc48b93d756425de6101
HamzaShafqat098 <hamzashafqat098@gmail.com>
2025-04-23 21:02:41 +0500

    Fixes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,19 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-          pathname: '/**',
-        },
-        {
-          protocol: 'http',
-          hostname: 'res.cloudinary.com',
-          pathname: '/**',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'flagsapi.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
